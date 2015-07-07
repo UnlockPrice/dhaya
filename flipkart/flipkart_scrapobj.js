3bfa731bbd8f7ba67150337f44ae712e7bfb392b
UnlockPrice <admin@unlockprice.com>
2015-07-07 23:33:04 +0530

    flipkart scrapobject
    flipkart scrapobject

---
+++ flipkart/flipkart_scrapobj.js
@@ -0,0 +1,8 @@
+exports.obj={
+laptops: {"brand":"brand","modelname":"modelname","modelid":"modelid","processor":"cpu","systemmemory":"ram","hddcapacity":"hdd","ssdcapacity":"ssd","operatingsystem":"os","graphicprocessor":"graphics","screentype":"screentype"},
+televisions: {"brand":"brand","screentype":"screentype","displaysize(cm)":"displaysize(cm)","displaysize(inch)":"displaysize(inch)","3d":"3d","modelname":"modelname","hdtechnology":"hdtechnology","resolution":"resolution","launchyear":"launchyear","smarttv":"smarttv"},
+mobiles: {"brand":"brand","keypad":"keypad","modelname":"modelname","modelid":"modelid","touchscreen":"touchscreen","simtype":"simtype","gprs":"gprs","3g":"3g","wifi":"wifi","bluetooth":"bluetooth","headsetdesign":"headsetdesign"},
+mobile_accessories:{"brand":"brand","model":"modelid","type":"type","cable":"cablelength"},
+men_cloting:{"type":"type","sleeve":"sleeve","occasion":"occasion","fabric":"fabric"},
+kitchen_appliances:{"brand":"brand","type":"type"}
+};
